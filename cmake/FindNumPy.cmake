ab78c3606936a4da7249a95307edea093133ab5d
Ganlin Zhang <zgl0315@gmail.com>
2025-08-27 17:18:03 +0200

    update

--- cmake/FindNumPy.cmake
+++ cmake/FindNumPy.cmake
@@ -3,7 +3,7 @@
 # PYTHON_NUMPY_FOUND
 # will be set by this script
 
-cmake_minimum_required(VERSION 3.5)
+cmake_minimum_required(VERSION 2.8)
 
 if(NOT PYTHON_EXECUTABLE)
     if(NumPy_FIND_QUIETLY)
